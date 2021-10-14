a2aed037a0ea811678766a08dc598fe5fdfb0e2e
kers04 <88822487+kers04@users.noreply.github.com>
2021-10-14 15:24:36 +0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("--****Hello World and Welcome to Jenkins Training****--");
+		System.out.println("--****Made Some Change****--");
 
 	}
 
